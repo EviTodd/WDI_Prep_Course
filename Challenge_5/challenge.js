449730059af2808b4d704dd221f43628d44fb475
Evi Todd <toddet96@gmail.com>
2017-01-02 11:24:23 -0500

    "Challenge 5 Writing JavaScript"

---
+++ Challenge_5/challenge.js
@@ -0,0 +1,44 @@
+// Exercise 1
+var name = 'Evi Todd';
+console.log('Hello ' + name);
+
+// Exercise 2
+var x = 1125;
+var answer = x % 2;
+console.log(answer);
+
+//Exercise 3
+var a = true || true;
+console.log(a);
+var b = false || false;
+console.log(b);
+var c = true || false;
+console.log(c);
+
+//Exercise 4
+var a = true && true;
+console.log(a);
+var b = true && false;
+console.log(b);
+var c = true && true;
+console.log(c);
+
+//Exercise 5
+var answer1 = 10 - 3 * 5/4 + 202 % 3;
+console.log(answer1);
+var answer2 = 10 - (3 * (5/4) + (202 % 3));
+console.log(answer2);
+
+//Exercsie 6
+(12).toString();
+parseInt('113');
+parseFloat('1.123456');
+var x = parseInt("Hello World", 10);
+console.log(x);
+
+
+
+
+
+
+
