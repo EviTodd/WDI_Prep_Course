b1477c6b658246e4a83e47f9061e25c95a9206cd
Evi Todd <toddet96@gmail.com>
2017-01-05 11:54:14 -0500

    "Challenge 7"

--- Challenge_6/challenge.js
+++ Challenge_6/challenge.js
@@ -10,17 +10,17 @@ Exercise #2
 Using the loop provided for you below, use an array's push method to insert the nummbers  0 to 10 into positions 0 through 10 of the numbers array you just initialized above.'If you're not sure how to use the array push method go back and re read this module on data strucutres.'
 */
 
-for(var i=0;i<11;i++){
-    numbers.push(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+for(var i = 1; i < 10; i++){
+    numbers.push(i);
 } 
 
 
-/*
+/* 
 Exercise #3
 Test that you used the array push method correctly by printing the sum of the values at position 3 and 6 of the array (use the console.log() function to print to the console). 
 The outputted value should be 9.
 */
-console.log(numbers[3 + 6]);
+console.log(numbers.length);
 
 
 
@@ -33,16 +33,17 @@ Use the console.log() function and the array .pop() to add all the values in the
 var sum = 0;
 
 while(numbers.length != 0){
-    numbers.pop();
+    numbers.pop(i);
 }
 console.log(sum);
+
 
 
 /*
 Exercise #5
 Create an empty object and assign it the variable car below.
 */
-var car = {color:"black"}
+var car = {color:"black"}; 
 console.log(car.color);
 
 /*
---
+++ Challenge_7/challenge.js
@@ -0,0 +1,23 @@
+
+//magic eightball
+
+var numberofreplys = 2;
+function ask(){
+    var choice = Math.floor(Math.random() * numberofreplys +1);
+    var reply = 'You need to make some options using if/else'
+
+    
+    if(choice >= 2){
+        console.log('There is a good chance.');
+    } else if (choice < 2){
+        console.log('Maybe');
+    }else {
+        console.log('Not in your wildest dreams.');
+    }
+
+
+
+    console.log(choice);
+}
+
+ask();
