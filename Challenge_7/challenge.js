8a9afdfec3deab4e420a49020929fde003f5ee37
Evi Todd <toddet96@gmail.com>
2017-01-08 13:50:39 -0500

    "Challenge 7 edit"

--- Challenge_7/challenge.js
+++ Challenge_7/challenge.js
@@ -4,17 +4,16 @@
 var numberofreplys = 2;
 function ask(){
     var choice = Math.floor(Math.random() * numberofreplys +1);
-    var reply = 'You need to make some options using if/else'
+       var reply = 'You need to make some options using if/else'
 
-    if(choice >= 2){
-        console.log('There is a good chance.');
-    } else if (choice < 2){
+    if(choice == 2){
+        console.log('There is a good chance');
+    } else if (choice <= 1){
         console.log('Maybe');
     }else {
-        console.log('Not in your wildest dreams.');
+        console.log('Not in your wildest dreams');
     }
         console.log(choice);
-       
     
 
     
