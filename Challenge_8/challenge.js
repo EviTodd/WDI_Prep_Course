5ca7613e9c3bafe93becc9f82bbd1747bccb3057
Evi Todd <toddet96@gmail.com>
2017-01-07 19:04:36 -0500

    "Challenge 8"

--- Challenge_8/challenge.js
+++ Challenge_8/challenge.js
@@ -18,11 +18,11 @@ for (var triangle = '#'; triangle.length < 8; triangle +='#'){
 //For every number that is a multiple of 3 print the word Buzz
 //If a number is a multiple of 5 and 3 (like 15) print the word FizzBuzz
 for (var i = 1; i <= 100; i++){
-    if(i % 5 == 0){
+    if(i % 3 == 0){
         console.log('Fizz');
-    }else if(i % 3 == 0){
+    }else if(i % 5 == 0){
         console.log('Buzz');
-    }else if (i % 5 == 0 && i % 5 ==0){
+    }else if (i % 5 == 0 && i % 3 ==0){
         console.log('FizzBuzz');
     }else{
         console.log(i);
@@ -34,8 +34,3 @@ for (var i = 1; i <= 100; i++){
 //Write a program that creates a string that represents an 8x8 grid, using newline characters to separate lines.
 //At each position of the grid there is either a space or a '#' character. 
 //The characters should form a chessboard.
-var chess = '#';
-var grid = 8;
-for(){
-    
-}
